acec939d714d8298ec11b13713ba8105e8068f8d
rbalynch <rbalynch@hotmail.com>
2023-10-30 10:45:19 -0500

    pop, get, getSize, getArraySize
    made 4 methods that fetch the top BufferedImage on the stack, a BufferedImage at an index, the current size of the available images on the stack, and the size of the stack itself

--- src/BufferedImageStack.java
+++ src/BufferedImageStack.java
@@ -1,4 +1,5 @@
 import java.awt.image.BufferedImage;
+import java.util.EmptyStackException;
 
 //Riley Lynch
 //Amar Nangia
@@ -15,4 +16,37 @@ public class BufferedImageStack
 		size = 0;
 		arr = new BufferedImage[2];
 	}
+	
+	public BufferedImage pop()
+	{
+		if (size == 0)
+		{
+			throw new EmptyStackException();
+		}
+		BufferedImage temp = arr[size - 1];
+		size--;
+		arr[size - 1] = null;
+		return temp;
+		
+	}
+	
+	public BufferedImage get(int index)
+	{
+		if (index > size)
+		{
+			throw new IndexOutOfBoundsException("ERROR");
+		}
+		return arr[index];
+	}
+	
+	public int getSize()
+	{
+		return size;
+	}
+	
+	public int getArraySize()
+	{
+		return arr.length;
+	}
+	
 }
